24249e195313572f56852e8100b1ddf4e1ee39df
bena <z8883sb.kilowatt483@passfwd.com>
2026-08-30 13:58:24 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+    description = "chest_looting_game development environment";
+
+    inputs = {
+
+        # Inputs the unstable branch of nixpkgs.
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    };
+
+    outputs = { nixpkgs, self }:
+
+        let
+
+            system = "x86_64-linux";
+            pkgs = import nixpkgs {
+                inherit system;
+            };
+
+        in
+
+            {
+
+            # Defines a development shell for this project.
+            devShells.${system}.default = pkgs.mkShell {
+
+                # Shell packages.
+                packages = with pkgs; [
+                    rustc
+                    cargo
+                    rustfmt
+                    clippy
+                ];
+
+                # Shell startup
+                # (this is optional, of course).
+                shellHook = /* bash */ ''
+                    clear && echo -e "\nchest_looting_game development environment\n"
+                    rustc --version
+                    cargo --version
+                    echo ""
+                '';
+
+            };
+
+        };
+
+}
